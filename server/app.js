001096e2310f7cc9f3c84552ef31735ce28d496e
bnGil <gilbn1994@gmail.com>
2022-06-26 11:56:18 +0300

    test

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.static(publicPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 
 app.get("/test", (req, res) => {
   try {
